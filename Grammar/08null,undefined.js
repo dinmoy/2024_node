c9afacb9ce514317bbb48a81e6ec9d1b7c1c75e2
dinmoy <imhyojin614@gmail.com>
2024-03-21 15:37:14 +0900

    null undefined의 비교

---
+++ Grammar/08null,undefined.js
@@ -0,0 +1,15 @@
+let foo 
+console.log(foo) //undefined
+
+const obj={}
+console.log(obj) //undefined
+
+console.log(bar)
+
+let bar=null 
+console.log(null) //null
+
+
+//주의할 것
+console.log(null == undefined)
+console.log(undefined == null)
